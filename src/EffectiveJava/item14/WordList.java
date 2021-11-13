29c48808d572267404c4e3bd039b407fc778adf4
lsmman <lsmman07@gmail.com>
2021-11-14 07:09:52 +0900

    chore(EffectiveJava): make launch.json to run

--- src/EffectiveJava/item14/WordList.java
+++ src/EffectiveJava/item14/WordList.java
@@ -1,12 +1,12 @@
 package EffectiveJava.item14;
-import java.math.BigDecimal;
+
 import java.util.*;
 
 // Comparable 구현 시의 이점 (87쪽)
 public class WordList {
     public static void main(String[] args) {
         Set<String> s = new TreeSet<>();
-//        Collections.addAll(s, );
+        // Collections.addAll(s, );
         s.add("hi");
         s.add("abc");
         s.add("zzz");
