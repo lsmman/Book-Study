402cf2904ab8982fa799a9b1bd5f654c58d2b8b1
lsmman <lsmman07@gmail.com>
2021-12-09 20:49:54 +0900

    feat(EffectiveJava): add code of item 36

---
+++ src/EffectiveJava/item36/Text.java
@@ -0,0 +1,26 @@
+package EffectiveJava.item36;
+
+import EffectiveJava.item35.Ensemble;
+
+import java.util.*;
+
+// 코드 36-2 EnumSet - 비트 필드를 대체하는 현대적 기법 (224쪽)
+public class Text {
+    public enum Style {BOLD, ITALIC, UNDERLINE, STRIKETHROUGH}
+
+    // 어떤 Set을 넘겨도 되나, EnumSet이 가장 좋다.
+    public void applyStyles(Set<Style> styles) {
+        System.out.printf("Applying styles %s to text%n",
+                Objects.requireNonNull(styles));
+    }
+
+    // 사용 예
+    public static void main(String[] args) {
+        Text text = new Text();
+        text.applyStyles(EnumSet.of(Style.BOLD, Style.ITALIC));
+
+        EnumSet<Style> bold = EnumSet.of(Style.BOLD, Style.ITALIC);
+        EnumSet<Style> allOfStyle = EnumSet.allOf(Style.class);
+        EnumSet<Style> range = EnumSet.range(Style.BOLD, Style.UNDERLINE);
+    }
+}
